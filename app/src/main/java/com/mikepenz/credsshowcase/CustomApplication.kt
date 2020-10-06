e2c9a9d974491e89c9a0270eed64a71fbd574d0a
Mike Penz <mikepenz@gmail.com>
2020-10-06 16:20:40 +0200

    - showcase ease of reverse engineering simple string key and secret

--- app/src/main/java/com/mikepenz/credsshowcase/CustomApplication.kt
+++ app/src/main/java/com/mikepenz/credsshowcase/CustomApplication.kt
@@ -1,10 +1,17 @@
 package com.mikepenz.credsshowcase
 
 import android.app.Application
+import com.mikepenz.credsshowcase.helper.FunSdk
 
 class CustomApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        // source
+        FunSdk.init("amazing-key", "super-secure-secret")
+
+        // resources
+        FunSdk.init(getString(R.string.key), getString(R.string.secret))
     }
 }
